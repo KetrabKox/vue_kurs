7e7722f21e06d7874719bf42f837731ed5d5192a
Bartek Firek <coox144@gmail.com>
2023-11-21 22:02:48 +0000

    Added Front and updated back - need to fix issue in be

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,61 @@
 const express = require('express');
 const httpServer = express();
 const dialer = require('dialer').Dialer;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
 
 const config = {
-    url: 'https://uni-call.fcc-online.pl',
-    login: '<login>',
-    password: '<haslo>'
+    url: process.env.VUE_APP_SERVER_URL,
+    login: process.env.LOGIN,
+    password: process.env.PASS,
+    myNumber: process.env.MY_NUMBER,
 };
 
 dialer.configure(config);
 
-httpServer.listen(3000, () =>{
-    console.log('Example app listening on port 3000!')
-} )
+// httpServer.listen(3000, () =>{
+//     console.log('Example app listening on port 3000!')
+// } )
 
-httpServer.get('/call/:number1/:number2', (req, res) => {
-    const number1 = req.params.number1;
-    const number2 = req.params.number2;
-    dialer.call(number1, number2);
-    res.json({success: true});
+// httpServer.get('/call/:number1/:number2', (req, res) => {
+//     const number1 = req.params.number1;
+//     const number2 = process.env.MY_NUMBER;
+//     dialer.call(number1, number2);
+//     res.json({success: true});
+// })
+
+httpServer.use(bodyParser.json());
+httpServer.use(cors());
+httpServer.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
+});
+
+httpServer.post('/call/', async (req, res) => {
+    const number1 = req.body.number;
+    const number2 = myNumber;
+    console.log('Dzwonie', number1, number2)
+    const bridge = await dialer.call(number1, number2);
+    let interval = setInterval(async () => {
+      let status = await bridge.getStatus();
+      console.log(status)
+      if (
+        status === "ANSWERED" ||
+        status === "FAILED" ||
+        status === "BUSY" ||
+        status === "NO ANSWER"
+      ) {
+        console.log("stop");
+        clearInterval(interval);
+      }
+    }, 2000);
+    res.json({ success: true });
+})
+
+httpServer.get('/status', async(req, res) => {
+    let status = await bridge.getStatus();
+    res.json({ id:'123', "status": status})
 })
