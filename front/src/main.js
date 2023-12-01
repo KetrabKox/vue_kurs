232125e2b383b5781605b3cf70b4e8597a2f953a
KetrabKox <coox144@gmail.com>
2023-12-01 20:07:58 +0100

    WWW Fixed views styles

--- front/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import ViewManager from "./services/ViewManager";
-
-ViewManager.checkStatus();
-createApp(App).use(router).use(router).mount("#app");
